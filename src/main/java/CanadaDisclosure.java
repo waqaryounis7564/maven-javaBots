e4698dd310ab57978559b6c748e3e320f186d926
Waqar <waqar.younas@2iqresearch.com>
2020-09-10 15:46:39 +0500

    Add:canada grabber completed

---
+++ src/main/java/CanadaDisclosure.java
@@ -0,0 +1,85 @@
+public class CanadaDisclosure {
+    private String reporting_date;
+    private String ticker;
+    private String issuer_name;
+    private String market;
+    private String short_selling_volume;
+    private String percent_of_total_traded_volume;
+    private String short_sell_traded_value;
+    private String percent_of_total_traded_value;
+    private String url;
+
+    public String getReporting_date() {
+        return reporting_date;
+    }
+
+    public void setReporting_date(String reporting_date) {
+        this.reporting_date = reporting_date;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getIssuer_name() {
+        return issuer_name;
+    }
+
+    public void setIssuer_name(String issuer_name) {
+        this.issuer_name = issuer_name;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+
+    public String getShort_selling_volume() {
+        return short_selling_volume;
+    }
+
+    public void setShort_selling_volume(String short_selling_volume) {
+        this.short_selling_volume = short_selling_volume;
+    }
+
+    public String getPercent_of_total_traded_volume() {
+        return percent_of_total_traded_volume;
+    }
+
+    public void setPercent_of_total_traded_volume(String percent_of_total_traded_volume) {
+        this.percent_of_total_traded_volume = percent_of_total_traded_volume;
+    }
+
+    public String getShort_sell_traded_value() {
+        return short_sell_traded_value;
+    }
+
+    public void setShort_sell_traded_value(String short_sell_traded_value) {
+        this.short_sell_traded_value = short_sell_traded_value;
+    }
+
+    public String getPercent_of_total_traded_value() {
+        return percent_of_total_traded_value;
+    }
+
+    public void setPercent_of_total_traded_value(String percent_of_total_traded_value) {
+        this.percent_of_total_traded_value = percent_of_total_traded_value;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+}
--- src/main/java/Canda.java
+++ src/main/java/Canda.java
@@ -29,65 +29,72 @@ import java.util.regex.Pattern;
 public class Canda {
 
     public static void scrap() throws IOException {
+        List<CanadaData> historicalData = new ArrayList<>();
         String url = "https://www.iiroc.ca/news/Pages/Short-Sale.aspx";
-        List<String> historicalUrls = new ArrayList<>(Arrays.asList(
-                "/Documents/2020/2d243746-e993-4711-aba0-31ca02c1e284_en.csv",
-                "/Documents/2020/cf59f5e3-f59c-4be6-a364-d757e936da22_en.csv",
-                "/Documents/2020/6604c07a-73fd-4523-8922-5500629e9477_en.csv",
-                "/Documents/2020/696e3956-dddf-4341-8a1e-5b516d022cbe_en.csv",
-                "/Documents/2020/0ca4115d-9181-4858-a343-37ca36dae5f5_en.csv",
-                "/Documents/2020/9fcd7ee9-a01a-48f6-94ad-bbe84fc3ba4f_en.csv",
-                "/Documents/2020/716bfeb2-64d5-49d6-93b7-2b80887ddd8b_en.csv",
-                "/Documents/2019/d153a3a0-6a0d-4e72-bde8-cbb51f0eccc5_en.csv",
-                "/Documents/2019/74547828-89a2-4c12-8681-a1ceab2f3404_en.csv",
-                "/Documents/2019/473839f8-52e6-4cff-893e-11837060eeb8_en.csv",
-                "/Documents/2019/c6119a10-c050-40f2-a93f-1ccd8ed6384d_en.csv",
-                "/Documents/2019/1eb3fc76-a051-43b4-8cfc-709e749d89d4_en.csv",
-                "/Documents/2019/f6e144a9-0211-443c-8cab-3cc34dc8c01e_en.csv",
-                "/Documents/2019/c805a1ce-4856-49e4-a955-547e70876b2c_en.csv",
-                "/Documents/2019/7c2fbb08-2d18-46fe-b996-41caf57cdbb7_en.csv",
-                "/Documents/2019/2f2aea15-c8e1-485a-899d-8a52edb8e4ba_en.csv",
-                "/Documents/2019/1606464b-1073-4eb1-bb81-006188f4cc4f_en.csv",
-                "/Documents/2019/8dc67e17-43ee-46f8-b68e-9aa8467ee6d8_en.csv",
-                "/Documents/2019/8950ee29-6722-4388-8cf8-4c81079f1d17_en.csv",
-                "/Documents/2019/4ae8891a-2da1-4c58-88b7-1bff50242d44_en.csv",
-                "/Documents/2019/604f5384-5058-455f-8ef3-e403cb3e0673_en.csv",
-                "/Documents/2019/38df84a3-b8a8-4c02-a065-524df584b8d8_en.csv",
-                "/Documents/2019/3d363df9-c75e-4de7-8544-6609a2e59867_en.csv",
-                "/Documents/2019/e7717bb9-5879-49ea-b849-c64066086650_en.csv",
-                "/Documents/2019/c6f13975-b217-45fb-994d-bd2986b4c65d_en.csv",
-                "/Documents/2019/400adb32-3236-4e8f-a4ad-045f2420c18b_en.csv",
-                "/Documents/2019/c919a728-bc61-47a0-9162-51b41d0d56f0_en.csv",
-                "/Documents/2019/4ca51e25-a476-42bd-bbd9-19f37b696def_en.csv",
-                "/Documents/2019/354758b0-861a-430f-b15b-aac77e0a2ee9_en.csv",
-                "/Documents/2019/d8277b02-66ea-4848-8c77-95d724f8a222_en.csv",
-                "/Documents/2019/c6cbfcae-d825-4df8-9c13-bb47160ba06b_en.csv",
-                "/Documents/2018/23e9ef21-cf54-43a5-920d-a09c234b9ea5_en.csv",
-                "/Documents/2019/c6cbfcae-d825-4df8-9c13-bb47160ba06b_en.csv",
-                "/Documents/2018/23e9ef21-cf54-43a5-920d-a09c234b9ea5_en.csv",
-                "/Documents/2018/e1b64c18-a435-4ef0-bc43-4cadacbdc48c_en.csv",
-                "/Documents/2018/6d5a9ce2-bc26-44fa-a8bc-3e709aec2f17_en.csv",
-                "/Documents/2018/2cd68f44-91f2-45cc-89fd-5cb4cd393b33_en.csv",
-                "/Documents/2018/0efb7bf6-6f7d-4b24-8cd4-3b9a164563d1_en.csv",
-                "/Documents/2018/05d40766-8096-4af2-a4a6-5acfadc1acf4_en.csv",
-                "/Documents/2018/4b001c0d-f902-4a63-9823-7a8ac20b5943_en.csv",
-                "/Documents/2018/fa284d99-ec57-43e8-b74f-0441cf4edbf8_en.csv",
-                "/Documents/2018/ccce241a-dd82-48d3-ad4b-295dd8627908_en.csv",
-                "/Documents/2018/8ad6611b-dafd-4d1a-b101-9a672faadd37_en.csv",
-                "/Documents/2018/b1c4f926-4f8d-4d8e-8da9-fe092f2995ae_en.csv",
-                "/Documents/2018/8e4426d0-f454-4856-a8ba-5c49219425aa_en.csv",
-                "/Documents/2018/64608250-a98c-40bd-88d3-b98c9780225e_en.csv",
-                "/Documents/2018/6a24bb95-b353-42dd-a645-bc678c53fc96_en.csv",
-                "/Documents/2018/06a7ff9a-77ac-4918-a6ea-aee9f2b811d6_en.csv",
-                "/Documents/2018/84d0b3e0-60e9-465b-b5af-b40d15fb8c02_en.csv",
-                "/Documents/2018/dafd165d-824a-42b9-a607-8341f57b1fe6_en.csv",
-                "/Documents/2018/91028f9c-f90b-4842-a4d0-ddb62122d036_en.csv",
-                "/Documents/2018/b6d5decc-0879-4646-bf20-3145cb65e956_en.csv",
-                "/Documents/2018/22e8e0d3-9e50-4a47-bc6f-d763a06f5847_en.csv",
-                "/Documents/2018/c88d8e3d-aaf0-415a-b233-613fa174a4d5_en.csv",
-                "/Documents/2018/8e4a7a8b-0a37-497d-b97e-fb41bfd76f36_en.csv"
-        ));
+        Map<String, String> urls = new HashMap<String, String>() {{
+            put("4/1/2020", "/Documents/2020/2d243746-e993-4711-aba0-31ca02c1e284_en.csv");
+            put("3/16/2020", "/Documents/2020/cf59f5e3-f59c-4be6-a364-d757e936da22_en.csv");
+            put("3/2/2020", "/Documents/2020/6604c07a-73fd-4523-8922-5500629e9477_en.csv");
+            put("2/17/2020", "/Documents/2020/696e3956-dddf-4341-8a1e-5b516d022cbe_en.csv");
+            put("2/3/2020", "/Documents/2020/0ca4115d-9181-4858-a343-37ca36dae5f5_en.csv");
+            put("1/16/2020", "/Documents/2020/9fcd7ee9-a01a-48f6-94ad-bbe84fc3ba4f_en.csv");
+            put("1/1/2020", "/Documents/2020/716bfeb2-64d5-49d6-93b7-2b80887ddd8b_en.csv");
+            put("12/16/2019", "/Documents/2019/d153a3a0-6a0d-4e72-bde8-cbb51f0eccc5_en.csv");
+            put("12/2/2019", "/Documents/2019/74547828-89a2-4c12-8681-a1ceab2f3404_en.csv");
+            put("11/18/2019", "/Documents/2019/473839f8-52e6-4cff-893e-11837060eeb8_en.csv");
+            put("11/1/2019", "/Documents/2019/c6119a10-c050-40f2-a93f-1ccd8ed6384d_en.csv");
+            put("10/16/2019", "/Documents/2019/1eb3fc76-a051-43b4-8cfc-709e749d89d4_en.csv");
+            put("10/1/2019", "/Documents/2019/f6e144a9-0211-443c-8cab-3cc34dc8c01e_en.csv");
+            put("9/16/2019", "/Documents/2019/c805a1ce-4856-49e4-a955-547e70876b2c_en.csv");
+            put("9/2/2019", "/Documents/2019/7c2fbb08-2d18-46fe-b996-41caf57cdbb7_en.csv");
+            put("8/16/2019", "/Documents/2019/2f2aea15-c8e1-485a-899d-8a52edb8e4ba_en.csv");
+            put("8/1/2019", "/Documents/2019/1606464b-1073-4eb1-bb81-006188f4cc4f_en.csv");
+            put("7/16/2019", "/Documents/2019/8dc67e17-43ee-46f8-b68e-9aa8467ee6d8_en.csv");
+            put("7/1/2019", "/Documents/2019/8950ee29-6722-4388-8cf8-4c81079f1d17_en.csv");
+            put("6/17/2019", "/Documents/2019/4ae8891a-2da1-4c58-88b7-1bff50242d44_en.csv");
+            put("6/3/2019", "/Documents/2019/604f5384-5058-455f-8ef3-e403cb3e0673_en.csv");
+            put("5/16/2019", "/Documents/2019/38df84a3-b8a8-4c02-a065-524df584b8d8_en.csv");
+            put("5/1/2019", "/Documents/2019/3d363df9-c75e-4de7-8544-6609a2e59867_en.csv");
+            put("4/16/2019", "/Documents/2019/e7717bb9-5879-49ea-b849-c64066086650_en.csv");
+            put("4/1/2019", "/Documents/2019/c6f13975-b217-45fb-994d-bd2986b4c65d_en.csv");
+            put("3/18/2019", "/Documents/2019/400adb32-3236-4e8f-a4ad-045f2420c18b_en.csv");
+            put("3/1/2019", "/Documents/2019/c919a728-bc61-47a0-9162-51b41d0d56f0_en.csv");
+            put("2/18/2019", "/Documents/2019/4ca51e25-a476-42bd-bbd9-19f37b696def_en.csv");
+            put("2/1/2019", "/Documents/2019/354758b0-861a-430f-b15b-aac77e0a2ee9_en.csv");
+            put("1/16/2019", "/Documents/2019/d8277b02-66ea-4848-8c77-95d724f8a222_en.csv");
+            put("1/1/2019", "/Documents/2019/c6cbfcae-d825-4df8-9c13-bb47160ba06b_en.csv");
+            put("12/17/2018", "/Documents/2018/23e9ef21-cf54-43a5-920d-a09c234b9ea5_en.csv");
+            put("12/3/2018", "/Documents/2018/e1b64c18-a435-4ef0-bc43-4cadacbdc48c_en.csv");
+            put("11/16/2018", "/Documents/2018/6d5a9ce2-bc26-44fa-a8bc-3e709aec2f17_en.csv");
+            put("11/1/2018", "/Documents/2018/2cd68f44-91f2-45cc-89fd-5cb4cd393b33_en.csv");
+            put("10/16/2018", "/Documents/2018/0efb7bf6-6f7d-4b24-8cd4-3b9a164563d1_en.csv");
+            put("10/1/2018", "/Documents/2018/05d40766-8096-4af2-a4a6-5acfadc1acf4_en.csv");
+            put("9/17/2018", "/Documents/2018/4b001c0d-f902-4a63-9823-7a8ac20b5943_en.csv");
+            put("9/3/2018", "/Documents/2018/2b36e9a4-58cd-4b44-a04e-5f717a2d3a76_en.csv");
+            put("8/16/2018", "/Documents/2018/fa284d99-ec57-43e8-b74f-0441cf4edbf8_en.csv");
+            put("8/1/2018", "/Documents/2018/ccce241a-dd82-48d3-ad4b-295dd8627908_en.csv");
+            put("7/16/2018", "/Documents/2018/8ad6611b-dafd-4d1a-b101-9a672faadd37_en.csv");
+            put("7/2/2018", "/Documents/2018/b1c4f926-4f8d-4d8e-8da9-fe092f2995ae_en.csv");
+            put("6/18/2018", "/Documents/2018/8e4426d0-f454-4856-a8ba-5c49219425aa_en.csv");
+            put("6/1/2018", "/Documents/2018/64608250-a98c-40bd-88d3-b98c9780225e_en.csv");
+            put("5/16/2018", "/Documents/2018/6a24bb95-b353-42dd-a645-bc678c53fc96_en.csv");
+            put("5/1/2018", "/Documents/2018/06a7ff9a-77ac-4918-a6ea-aee9f2b811d6_en.csv");
+            put("4/16/2018", "/Documents/2018/84d0b3e0-60e9-465b-b5af-b40d15fb8c02_en.csv");
+            put("4/2/2018", "/Documents/2018/dafd165d-824a-42b9-a607-8341f57b1fe6_en.csv");
+            put("3/16/2018", "/Documents/2018/91028f9c-f90b-4842-a4d0-ddb62122d036_en.csv");
+            put("3/1/2018", "/Documents/2018/b6d5decc-0879-4646-bf20-3145cb65e956_en.csv");
+            put("2/16/2018", "/Documents/2018/22e8e0d3-9e50-4a47-bc6f-d763a06f5847_en.csv");
+            put("2/1/2018", "/Documents/2018/c88d8e3d-aaf0-415a-b233-613fa174a4d5_en.csv");
+            put("1/16/2018", "/Documents/2018/8e4a7a8b-0a37-497d-b97e-fb41bfd76f36_en.csv");
+        }};
+        for (Map.Entry<String, String> entry : urls.entrySet()) {
+            CanadaData data = new CanadaData();
+            data.setDate(entry.getKey());
+            data.setUrl(entry.getValue());
+            historicalData.add(data);
+        }
 
+//historicalData.forEach(t-> System.out.println(t.getDate() + " : " +t.getUrl()));
         try (WebClient webClient = new WebClient()) {
             URL ur = new URL(url);
             WebRequest requestSettings = new WebRequest(ur, HttpMethod.POST);
@@ -98,18 +105,49 @@ public class Canda {
             Document document = Jsoup.parse(mainPage.asXml());
             Elements tbody = document.select("#WebPartWPQ2 > table:nth-child(1) > tbody >tr >" +
                     "td:nth-child(1),#WebPartWPQ2 > table:nth-child(1) > tbody >tr >td:nth-child(2)");
+//            System.out.println(tbody);
+            String[] rows = String.valueOf(tbody).split("</td>");
+            for (int i = 0; i < rows.length - 1; ) {
+                urls.put(getDate(rows[i++]), getUrl(rows[i++]));
+            }
+//            for (Map.Entry<String, String> entry : historicalData.entrySet()) {
+////                downloadUrl(entry.getKey(), "https://www.iiroc.ca" + entry.getValue());
+//                System.out.println("Key = " + getDate(entry.getKey()) + ", Value = " + getUrl(entry.getValue()));
+//            }
 
-            String[] rr = String.valueOf(tbody).split("</td>");
-            for (String line : rr) {
-                if (getDate(line) == null) {
-                    String href = getUrl(line);
-                    System.out.println(href);
-                } else {
-                   String  date =getDate(line);
-                    System.out.println(date);
-                }
+//            int j=1;
+//            for (int i=0;i<rr.length;i++){
+//                if(j==20){
+//                    j=19;
+//                }
+//                urls.put(rr[i],rr[j]);
+//                j++;
+//            }
+
+            for (Map.Entry<String, String> entry : urls.entrySet()) {
+                downloadUrl(entry.getKey(), "https://www.iiroc.ca" + entry.getValue());
+//                System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
             }
 
+//            for (String line : rr) {
+//                System.out.println(line);
+//                CanadaData data=new CanadaData();
+//                if (getDate(line) == null) {
+//                    String href = getUrl(line);
+//
+//                    System.out.println(href);
+//                    //                    data.setUrl(href);
+//                } else {
+//                   String  date =getDate(line);
+//                    System.out.println(date);
+////                    data.setDate(date);
+//                }
+//
+//            }
+
+//
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
 
@@ -138,7 +176,7 @@ public class Canda {
         return date;
     }
 
-    private static void downloadUrl(String link) throws IOException {
+    private static void downloadUrl(String date, String link) throws IOException, ParseException {
         HttpURLConnection httpURLConnection = (HttpURLConnection) (new URL(link).openConnection());
         httpURLConnection.addRequestProperty("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36");
         if (httpURLConnection.getResponseCode() == 404) return;
@@ -150,16 +188,44 @@ public class Canda {
 
                     String line = bufferedReader.readLine();
                     while (line != null) {
-                        if (line.startsWith("Product")) {
-                            line = bufferedReader.readLine();
-                            continue;
-                        }
-                        System.out.println(line);
+                        processCsvLine(date, link, line);
+                        ;
                         line = bufferedReader.readLine();
                     }
                 }
             }
         }
+    }
+
+    private static void processCsvLine(String date, String url, String line) throws ParseException {
+        String[] anns = line.split(",");
+        CanadaDisclosure disclosure = new CanadaDisclosure();
+        SimpleDateFormat sdf1 = new SimpleDateFormat("M/d/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd");
+        Date parseDate = sdf1.parse(date);
+
+
+        disclosure.setReporting_date(sdf2.format(parseDate));
+        disclosure.setTicker(anns[0]);
+        disclosure.setIssuer_name(anns[1]);
+        disclosure.setMarket(anns[2]);
+        disclosure.setShort_selling_volume(anns[3]);
+        disclosure.setPercent_of_total_traded_volume(anns[4]);
+        disclosure.setShort_sell_traded_value(anns[5]);
+        disclosure.setPercent_of_total_traded_value(anns[6]);
+        disclosure.setUrl(url);
+        System.out.println(disclosure.getReporting_date());
+        System.out.println(disclosure.getTicker());
+        System.out.println(disclosure.getIssuer_name());
+        System.out.println(disclosure.getMarket());
+        System.out.println(disclosure.getShort_selling_volume());
+        System.out.println(disclosure.getPercent_of_total_traded_volume());
+        System.out.println(disclosure.getShort_sell_traded_value());
+        System.out.println(disclosure.getPercent_of_total_traded_value());
+        System.out.println(disclosure.getUrl());
+        // call db
+        System.out.println("*************");
+
     }
 }
 /**
