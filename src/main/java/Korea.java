193010726216cc70c5ab40d66c6c28245c42a673
Waqar <waqar.younas@2iqresearch.com>
2020-09-17 15:02:41 +0500

    Add:Korea grabber completed

--- src/main/java/Korea.java
+++ src/main/java/Korea.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -25,7 +26,7 @@ public class Korea {
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
         String inputLine;
-        StringBuffer response = new StringBuffer();
+        StringBuilder response = new StringBuilder();
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
 
@@ -35,12 +36,14 @@ public class Korea {
 
     }
 
-    public static void scrapeData() throws IOException {
+    public static void scrapeData() throws IOException, ParseException {
         String url = "";
-        for (int i = 0; i <= 6; i++) {
-            String dateStart = new SimpleDateFormat("yyyy-MM-dd").format(ParameterUtils.getDateBeforeDays(new Date(), i));
-            for (int j = 1; j <= 5; j++) {
-                url = "https://kind.krx.co.kr/disclosure/todaydisclosure.do?method=searchTodayDisclosureSub&currentPageSize=100&pageIndex=" + j + "&orderMode=0&orderStat=D&marketType=&forward=todaydisclosure_sub&searchMode=&searchCodeType=&chose=S&todayFlag=Y&repIsuSrtCd=&selDate=" + dateStart + "&searchCorpName=";
+        int maxDays = 6;
+        int maxPage = 5;
+        for (int day = 0; day <= maxDays; day++) {
+            String dateStart = new SimpleDateFormat("yyyy-MM-dd").format(ParameterUtils.getDateBeforeDays(new Date(), day));
+            for (int page = 1; page <= maxPage; page++) {
+                url = "https://kind.krx.co.kr/disclosure/todaydisclosure.do?method=searchTodayDisclosureSub&currentPageSize=100&pageIndex=" + page + "&orderMode=0&orderStat=D&marketType=&forward=todaydisclosure_sub&searchMode=&searchCodeType=&chose=S&todayFlag=Y&repIsuSrtCd=&selDate=" + dateStart + "&searchCorpName=";
                 Document document = Jsoup.parse(downloadPage(url));
                 int tableIsEmpty = document.select("tbody>tr").size();
                 if (tableIsEmpty == 1) continue;
@@ -49,19 +52,52 @@ public class Korea {
         }
     }
 
-    private static void processDocument(Document document, String date) {
+    private static void processDocument(Document document, String date) throws ParseException {
         System.out.println(date);
         Elements rows = document.select("tbody>tr");
+        String prefixUrl = "http://kind.krx.co.kr/common/disclsviewer.do?method=search&acptno=";
+        String postFixUrl = "&docno=&viewerhost=&viewerport=";
+        String dateTime = "";
+        String companyName = "";
+        String heading = "";
+        String rnsId = "";
+        String savingUrl = "";
+        String releasedTime = "";
         for (Element row : rows) {
-            System.out.println("date: " + row.select("td.first.txc").text());
-            System.out.println("companyName: " + row.select("#companysum").text());
-            System.out.println("Heading: " + row.select("td:nth-child(3) > a").text());
+            dateTime = row.select("td.first.txc").text();
+//           System.out.println("date: " + date + dateTime);
+            companyName = row.select("#companysum").text();
+//            System.out.println("companyName: " + companyName);
+            heading = row.select("td:nth-child(3) > a").text();
+//            System.out.println("Heading: " + heading);
+            rnsId = row.select("td:nth-child(3) > a").attr("onClick").split("'")[1];
+            savingUrl = prefixUrl + rnsId + postFixUrl;
+            releasedTime = date + " " + dateTime;
+//            System.out.println("popUp : " + savingUrl);
+            if (heading.contains("자기주식")) {
+//                System.out.println("company Name is :"+companyName);
+//                System.out.println(savingUrl);
+                KoreaDisclosure disclosure = new KoreaDisclosure();
+                disclosure.setCompanyName(companyName);
+                disclosure.setHeadline(heading);
+                disclosure.setRnsId(rnsId);
+                disclosure.setSavingUrl(savingUrl);
+                Date parsedTime = new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(releasedTime);
+                dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(parsedTime);
+                disclosure.setDateTime(dateTime);
+                System.out.println(disclosure.getHeadline());
 
-            System.out.println("popUp : ");
+            }
+
             System.out.println("********************************************************");
         }
 
 
         System.out.println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+    }
+
+    private static void saveToDB(KoreaDisclosure disclosure) {
+//        if (!botsRepo.checkIsUrlAlreadyExist(disclosure.getSavingUrl(), countryId)) {
+//            botsRepo.insertAnnouncement(disclosure.getHeadline(),disclosure.getDateTime(),14,3,disclosure.getSavingUrl(),countryId,disclosure.getCompanyName(),disclosure.getRnsId())
     }
 }
---
+++ src/main/java/KoreaDisclosure.java
@@ -0,0 +1,47 @@
+public class KoreaDisclosure {
+    private String   dateTime;
+    private String companyName;
+    private String headline;
+    private String savingUrl;
+    private String rnsId;
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getSavingUrl() {
+        return savingUrl;
+    }
+
+    public void setSavingUrl(String savingUrl) {
+        this.savingUrl = savingUrl;
+    }
+
+    public String getRnsId() {
+        return rnsId;
+    }
+
+    public void setRnsId(String rnsId) {
+        this.rnsId = rnsId;
+    }
+}
