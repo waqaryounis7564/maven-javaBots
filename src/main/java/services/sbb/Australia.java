f13f468ef7c742dc9468ea914923bf78d543f832
Waqar <waqar.younas@2iqresearch.com>
2020-11-27 10:50:29 +0500

    feat(Australia):json iterator method implemented

--- src/main/java/services/sbb/Australia.java
+++ src/main/java/services/sbb/Australia.java
@@ -2,21 +2,48 @@ package services.sbb;
 
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import utils.ParameterUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class Australia {
+    private static ArrayList<String> keyWords = new ArrayList<>(Arrays.asList(
+            "buy-back",
+            "BUYBACK",
+            "FORM 484",
+            "FORM-484",
+            "3C",
+            "3D",
+            "3E",
+            "3F",
+            "APPENDIX 3E",
+            "APPENDIX-3E",
+            "CAPITAL RETURN",
+            "cancellation of shares",
+            "Share Cancellation",
+            "Ordinary Shares"
+    ));
+
+
     public static void scrapeData() throws IOException {
         URL url = new URL("https://asx.api.markitdigital.com/asx-research/1.0/markets/announcements?itemsPerPage=1000&summaryCountsDate=2020-11-23&includeFacets=true");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestProperty("Authorization", "Bearer 83ff96335c2d45a094df02a206a39ff4");
         conn.setRequestProperty("Content-Type", "application/json");
         conn.setRequestMethod("GET");
+        int statusCode = conn.getResponseCode();
+        if (statusCode != 200)
+            throw new RuntimeException(" NO RESPONSE AVAILABLE FOR AUSTRALIA, STATUS CODE" + statusCode);
         BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         String output;
 
@@ -30,14 +57,33 @@ public class Australia {
         JSONArray jsonArray = jsonObject.
                 getJSONObject("data").
                 getJSONArray("items");
+        Iterator<Object> iterator = jsonArray.iterator();
+        int count=1;
+        while (iterator.hasNext()) {
+            JSONObject obj = (JSONObject) iterator.next();
+            try {
+                String ticker = obj
+                        .getJSONArray("companyInfo")
+                        .getJSONObject(0)
+                        .getString("symbol");
+                String companyName = obj
+                        .getJSONArray("companyInfo")
+                        .getJSONObject(0)
+                        .getString("displayName");
+                String date = obj.getString("date");
+                String documentKey = obj.getString("documentKey");
+                String headLine = obj.getString("headline");
+                String annsUrl = "https://cdn-api.markitdigital.com/apiman-gateway/ASX/asx-research/1.0/file/" + documentKey + "?access_token=83ff96335c2d45a094df02a206a39ff4";
+                date = ParameterUtils.getDateInYourFormat(date, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "yyyy-MM-dd HH:mm:ss");
+                count++;
+                System.out.println(
+                        MessageFormat.format("{0},{1},{2},{3}::::{4}::::::", ticker, headLine, annsUrl, documentKey,count));
 
-     for (int i=0;i<jsonArray.length();i++){
-         String string = jsonArray.getJSONObject(i).
-                 getJSONArray("companyInfo")
-                 .getJSONObject(0).
-                         getString("symbol");
-         System.out.println(string);
-     }
+            } catch (JSONException ex) {
+                ex.getCause();
+            }
+        }
+
 
     }
 }
