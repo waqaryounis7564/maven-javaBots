76b775bec38b5d9e4f5baa8c99676519567c4ca3
Waqar <waqar.younas7564@gmail.com>
2020-12-03 08:33:54 +0500

    timezone implmented

--- src/main/java/services/BR.java
+++ src/main/java/services/BR.java
@@ -1,160 +1,72 @@
-//package services;
-//
-//
-//import com.gargoylesoftware.htmlunit.HttpMethod;
-//import com.gargoylesoftware.htmlunit.WebClient;
-//import com.gargoylesoftware.htmlunit.WebRequest;
-//import com.gargoylesoftware.htmlunit.html.DomElement;
-//import com.gargoylesoftware.htmlunit.html.HtmlPage;
-//import com.gargoylesoftware.htmlunit.util.NameValuePair;
-//import org.apache.http.HttpResponse;
-//import org.apache.http.client.methods.HttpPost;
-//import org.apache.http.conn.ssl.NoopHostnameVerifier;
-//import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-//import org.apache.http.impl.client.CloseableHttpClient;
-//import org.apache.http.impl.client.HttpClientBuilder;
-//import org.apache.http.impl.client.HttpClients;
-//import org.apache.http.ssl.SSLContextBuilder;
-//import org.apache.http.ssl.SSLContexts;
-//import utils.RequestSetter;
-//
-//import javax.net.ssl.HttpsURLConnection;
-//import javax.net.ssl.SSLContext;
-//import javax.net.ssl.SSLSocketFactory;
-//import java.io.*;
-//import java.net.HttpURLConnection;
-//import java.net.URL;
-//import java.net.URLEncoder;
-//import java.nio.charset.StandardCharsets;
-//import java.security.KeyManagementException;
-//import java.security.KeyStoreException;
-//import java.security.NoSuchAlgorithmException;
-//import java.security.cert.CertificateException;
-//import java.util.ArrayList;
-//import java.util.LinkedHashMap;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.zip.GZIPInputStream;
-//
-//import static utils.ParameterUtils.getSocketFactory;
-//
-//public class BR {
-////    final WebClient webClient = getDomNodeOrDie().getPage().getEnclosingWindow().getWebClient();
-////  webClient.getPage(webClient.getOptions().getHomePage());
-//
-//    public static void scrapeDate() throws IOException {
-//        try (WebClient webClient = new WebClient()) {
-//            String url="https://www.rad.cvm.gov.br/ENET/frmConsultaExternaCVM.aspx";
-//            webClient.getOptions().setCssEnabled(false);
-//            webClient.getOptions().setJavaScriptEnabled(true);
-//            webClient.getOptions(). setUseInsecureSSL(true);
-//            webClient.getOptions().setThrowExceptionOnScriptError(false);
-//
-//            final HtmlPage page = webClient.getPage(url);
-//            DomElement submitBtn=page.querySelector("#btnConsulta");
-//            HtmlPage pg2=submitBtn.click();
-//            webClient.waitForBackgroundJavaScript(10000);
-//            String res=pg2.asXml();
-//            System.out.println(res);
-//
-//        }
-//    }
-//
-//    public static void scrape() throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, KeyManagementException {
-//
-//        SSLContextBuilder SSLBuilder = SSLContexts.custom();
-//        File file = new File("C:\\Program Files (x86)\\Java\\jre1.8.0_261\\lib\\security\\keystore.cer");
-//        SSLBuilder = SSLBuilder.loadTrustMaterial(file,
-//                "changeit".toCharArray());
-//        SSLContext sslcontext = SSLBuilder.build();
-//        SSLConnectionSocketFactory sslConSocFactory = new SSLConnectionSocketFactory(sslcontext, new NoopHostnameVerifier());
-//        HttpClientBuilder clientbuilder = HttpClients.custom();
-//        clientbuilder.setSSLSocketFactory(sslConSocFactory);
-//        CloseableHttpClient httpclient = clientbuilder.build();
-//        HttpPost httpPost = new HttpPost("https://www.rad.cvm.gov.br/ENET/frmConsultaExternaCVM.aspx/ListarDocumentos");
-//        HttpResponse httpresponse = httpclient.execute(httpPost);
-//        System.out.println(httpresponse.getStatusLine());
-//    }
-//
-//    public static void scraping() throws IOException {
-//
-//        String cookie = "  _ga=GA1.3.1352294763.1601901539; _gid=GA1.3.1673770607.1602480500; rxVisitor=16025006608559H4ET8ATDGTCJBBDVBIVB2GBKCG4EV0V; ASP.NET_SessionId=ujoeagmggb1nto55wlxhpl55; BIGipServerpool_www.rad.cvm.gov.br_443=1174939658.47873.0000; TS01871345=016e3b076f5ae2dedc2645da6ed4670aaf36243ec0d1616a1149a9bb5444a5257f1b47964f36a47347387ec2d9c122e3753711c804048153c1609c0996d5d932f6b4d7e626; dtCookie=v_4_srv_1_sn_2M09H77LCU1F2FGR661PRNGU1VTRH64T_perc_100000_ol_0_mul_1_app-3Ad94293f33bf4a8d1_1; TS01f82b11=016e3b076f50d9d56eb3fe7092605b6259be6fc945d1616a1149a9bb5444a5257f1b47964ff27b9fbb166b6d21a2d89f9af45c46f7c0d6cf4423abe9be918259e3725dbb02; dtSa=-; dtLatC=2; rxvt=1602504720592|1602502504176; dtPC=1$502920138_431h3vQMMBHMMCMBRMELEUFTFWAKPRJJFHKAVO-0e1";
-//        String accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9";
-//        final String acceptEncoding = "gzip, deflate, br";
-//        final String acceptLanguage = "en-US,en;q=0.9,ur;q=0.8,de;q=0.7,el;q=0.6";
-//        String url = "https://www.rad.cvm.gov.br/ENET/frmConsultaExternaCVM.aspx/ListarDocumentos";
-//        Map<String, Object> params = new LinkedHashMap<>();
-//        params.put("dataDe", "01/10/2020");
-//        params.put("dataAte", "14/10/2020");
-//        params.put("empresa", "");
-//        params.put("setorAtividade", "-1");
-//        params.put("categoriaEmissor", "-1");
-//        params.put("situacaoEmissor", "-1");
-//        params.put("tipoDocumento", "-1");
-//        params.put("dataReferencia", "");
-//        params.put("categoria", "8");
-//        params.put("tipo", "99");
-//        params.put("especie", "-1");
-//        params.put("periodo", "2");
-//        params.put("horaIni", "");
-//        params.put("horaFim", "");
-//        params.put("palavraChave", "");
-//        params.put("ultimaDtRef", "false");
-//        params.put("tipoEmpresa", "0");
-//        StringBuilder postData = new StringBuilder();
-//        String postData1="{ dataDe: '', dataAte: '' , empresa: '', setorAtividade: '-1', categoriaEmissor: '-1', situacaoEmissor: '-1', tipoDocumento: '-2', dataReferencia: '', categoria: '-1', tipo: '-1', especie: '-1', periodo: '0', horaIni: '', horaFim: '', palavraChave:'',ultimaDtRef:'false', tipoEmpresa:'0'}";
-////        for (Map.Entry<String, Object> param : params.entrySet()) {
-////            if (postData.length() != 0) postData.append("&");
-////            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-////            postData.append("=");
-////            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-////        }
-//        byte[] postDataBytes = postData1.toString().getBytes("UTF-8");
-//
-//
-//        HttpsURLConnection httpsURLConnection = (HttpsURLConnection) new URL(url).openConnection();
-//        SSLSocketFactory sslSocketFactory = getSocketFactory();
-//        if (sslSocketFactory != null)
-//            httpsURLConnection.setSSLSocketFactory(sslSocketFactory);
-//        httpsURLConnection.setRequestMethod("POST");
-//        RequestSetter requestSetter = new RequestSetter();
-//        requestSetter.setAccept(accept);
-//        requestSetter.setHost("www.rad.cvm.gov.br");
-//        requestSetter.setOrigin("https://www.rad.cvm.gov.br");
-//        requestSetter.setAcceptEncoding(acceptEncoding);
-//        requestSetter.setAcceptLanguage(acceptLanguage);
-//        requestSetter.setReferer("https://www.rad.cvm.gov.br/ENET/frmConsultaExternaCVM.aspx");
-//        requestSetter.setContentLength(String.valueOf(postData.length()));
-//        httpsURLConnection.setDoOutput(true);
-//        httpsURLConnection = requestSetter.getHttpsUrlConnection(httpsURLConnection);
-//        httpsURLConnection.addRequestProperty("cookie", cookie);
-//
-//
-//        try (OutputStream outputStream = httpsURLConnection.getOutputStream()) {
-//            outputStream.write(postDataBytes);
-//            getResponseFromHttpConnection(httpsURLConnection);
-//        }
-//
-//
-//    }
-//
-//    private static String getResponseFromHttpConnection(HttpURLConnection httpURLConnection) throws IOException {
-//        try (InputStream inputStream = httpURLConnection.getInputStream()) {
-//            Reader reader;
-//            if ("gzip".equals(httpURLConnection.getContentEncoding())) {
-//                reader = new InputStreamReader(new GZIPInputStream(inputStream));
-//            } else {
-//                reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-//            }
-//            try (BufferedReader br = new BufferedReader(reader)) {
-//                StringBuilder sb = new StringBuilder();
-//                String line;
-//                while ((line = br.readLine()) != null) {
-//                    sb.append(line).append("\n");
-//                    System.out.println(line);
-//                }
-//                return sb.toString();
-//            }
-//        }
-//    }
-//}
+package services;
+
+import utils.ParameterUtils;
+
+import java.text.MessageFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class BR {
+    public static void scrape() throws ParseException {
+
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTime(new Date());
+        String time = "2020-11-30T21:16:23.000Z";
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+//        Date parsed = sdf.parse(time);
+
+//        sdf.setTimeZone(TimeZone.getTimeZone("Etc/GMT-10"));
+
+//        String dt = ParameterUtils.getDateInYourFormat(sdf.format(parsed), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "yyyy-MM-dd hh:mm:ss a");
+//        System.out.println("Australia :" + dt);
+//        Arrays.stream(TimeZone.getAvailableIDs()).forEach(p-> System.out.println(p));
+//Here you set to your timezone
+//        sdf.setTimeZone(TimeZone.getDefault());
+//        String pt = ParameterUtils.getDateInYourFormat(sdf.format(parsed), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "yyyy-MM-dd hh:mm:ss a");
+//        System.out.println("Pakistan :" + pt);
+//        System.out.println("in db "+pt);
+
+String j=ni(time);
+        System.out.println(j);
+
+}
+private static String ni(String date){
+    Date parsed = null;
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    try {
+        parsed = sdf.parse(date);
+    } catch (ParseException e) {
+//        logger.error(e.getMessage());
+    }
+    sdf.setTimeZone(TimeZone.getTimeZone("Etc/GMT-10"));
+    return ParameterUtils.getDateInYourFormat(sdf.format(parsed), "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "yyyy-MM-dd HH:mm:ss");
+}
+}
+
+//Australia/ACT
+//        Australia/Adelaide
+//        Australia/Brisbane
+//        Australia/Broken_Hill
+//        Australia/Canberra
+//        Australia/Currie
+//        Australia/Darwin
+//        Australia/Eucla
+//        Australia/Hobart
+//        Australia/LHI
+//        Australia/Lindeman
+//        Australia/Lord_Howe
+//        Australia/Melbourne
+//        Australia/NSW
+//        Australia/North
+//        Australia/Perth
+//        Australia/Queensland
+//        Australia/South
+//        Australia/Sydney
+//        Australia/Tasmania
+//        Australia/Victoria
+//        Australia/West
+//        Australia/Yancowinna
+
--- src/main/java/services/Denmark.java
+++ src/main/java/services/Denmark.java
@@ -1,99 +1,99 @@
-package services;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import utils.ParameterUtils;
-
-import javax.net.ssl.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-
-public class Denmark {
-    private static int count = 0;
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    public static void startDKBot() throws IOException {
-        getDKAnnouncements();
-    }
-
-    private static String getPageContent(String link) throws Exception {
-        URL url = new URL(link);
-        URLConnection urlConnection = url.openConnection();
-        HttpsURLConnection httpsUrlConnection = (HttpsURLConnection) urlConnection;
-        SSLSocketFactory sslSocketFactory = createSslSocketFactory();
-        httpsUrlConnection.setSSLSocketFactory(sslSocketFactory);
-        try (InputStream inputStream = httpsUrlConnection.getInputStream()) {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            StringBuilder line = new StringBuilder();
-            String line1;
-            while ((line1 = reader.readLine()) != null) {
-                line.append(line1);
-            }
-            return line.toString();
-        }
-    }
-
-    private static SSLSocketFactory createSslSocketFactory() throws Exception {
-        TrustManager[] byPassTrustManagers = new TrustManager[]{new X509TrustManager() {
-            public X509Certificate[] getAcceptedIssuers() {
-                return new X509Certificate[0];
-            }
-
-            public void checkClientTrusted(X509Certificate[] chain, String authType) {
-            }
-
-            public void checkServerTrusted(X509Certificate[] chain, String authType) {
-            }
-        }};
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(null, byPassTrustManagers, new SecureRandom());
-        return sslContext.getSocketFactory();
-    }
-
-    private static void getDKAnnouncements() throws IOException {
-        count = 0;
-        int index = 1;
-        int sindex = 1;
-        int totalPages = 44;
-        String dateEnd = new SimpleDateFormat("yyyy:MM:dd").format(new Date());
-        String dateStart = new SimpleDateFormat("yyyy:MM:dd").format(ParameterUtils.getDateBeforeDays(new Date(), 28));
-        Document response;
-        for (int pageNumber = 0; pageNumber <= totalPages; pageNumber++) {
-            String url = "https://oasm.dfsa.dk/uk/searchannouncementresult.aspx?headline=&name=&RealAnnouncerCVR=&CVRnumber=&announcmentsId=-1&informationTypeId=-1&announcementTypeId=-1&languageId=-1&nationality=-1&pubDateFrom=" + dateStart + ":00:00&pubDateTo=" + dateEnd + ":12:21&regDateFrom=1999:01:01:00:00&regDateTo=2050:01:01:00:00&index=" + index + "&sindex=" + sindex + "&historic=1&postponed=0";
-            String testUrl = "https://oasm.dfsa.dk/uk/searchannouncementresult.aspx?headline=&name=&CVRnumber=&RealAnnouncerCVR=&announcmentsId=-1&informationTypeId=-1&announcementTypeId=-1&languageId=-1&nationality=-1&pubDateFrom=2020:10:15:00:00&pubDateTo=2020:11:12:12:21&regDateFrom=1999:01:01:00:00&regDateTo=2050:01:01:00:00&index=7&sindex=61&historic=1&postponed=0";
-            try {
-
-                Document document = Jsoup.parse(getPageContent(testUrl));
-                Elements rows = document.select("table>tbody");
-                if(rows.size()==0) break;
-
-                for (Element row : rows) {
-                    String select = row.select("tr> td:nth-child(1)").text();
-                    String announcement = row.select("tr.detail> td:nth-child(3) > strong > a").text();
-                    String announcementLink = row.select("tr.detail > td:nth-child(3)> strong > a").attr("href");
-                    String company = row.select(" tr.detail > td:nth-child(4)").text();
-                    String g = "gh";
-                }
-                index += 1;
-                sindex += 10;
-            } catch (Exception e) {
-                return;
-            }
-        }
-
-
-    }
-}
+//package services;
+//
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import utils.ParameterUtils;
+//
+//import javax.net.ssl.*;
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.net.URL;
+//import java.net.URLConnection;
+//import java.security.SecureRandom;
+//import java.security.cert.X509Certificate;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+//
+//public class Denmark {
+//    private static int count = 0;
+//    private Logger logger = LoggerFactory.getLogger(this.getClass());
+//
+//    public static void startDKBot() throws IOException {
+//        getDKAnnouncements();
+//    }
+//
+//    private static String getPageContent(String link) throws Exception {
+//        URL url = new URL(link);
+//        URLConnection urlConnection = url.openConnection();
+//        HttpsURLConnection httpsUrlConnection = (HttpsURLConnection) urlConnection;
+//        SSLSocketFactory sslSocketFactory = createSslSocketFactory();
+//        httpsUrlConnection.setSSLSocketFactory(sslSocketFactory);
+//        try (InputStream inputStream = httpsUrlConnection.getInputStream()) {
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//            StringBuilder line = new StringBuilder();
+//            String line1;
+//            while ((line1 = reader.readLine()) != null) {
+//                line.append(line1);
+//            }
+//            return line.toString();
+//        }
+//    }
+//
+//    private static SSLSocketFactory createSslSocketFactory() throws Exception {
+//        TrustManager[] byPassTrustManagers = new TrustManager[]{new X509TrustManager() {
+//            public X509Certificate[] getAcceptedIssuers() {
+//                return new X509Certificate[0];
+//            }
+//
+//            public void checkClientTrusted(X509Certificate[] chain, String authType) {
+//            }
+//
+//            public void checkServerTrusted(X509Certificate[] chain, String authType) {
+//            }
+//        }};
+//        SSLContext sslContext = SSLContext.getInstance("TLS");
+//        sslContext.init(null, byPassTrustManagers, new SecureRandom());
+//        return sslContext.getSocketFactory();
+//    }
+//
+//    private static void getDKAnnouncements() throws IOException {
+//        count = 0;
+//        int index = 1;
+//        int sindex = 1;
+//        int totalPages = 44;
+//        String dateEnd = new SimpleDateFormat("yyyy:MM:dd").format(new Date());
+//        String dateStart = new SimpleDateFormat("yyyy:MM:dd").format(ParameterUtils.getDateBeforeDays(new Date(), 28));
+//        Document response;
+//        for (int pageNumber = 0; pageNumber <= totalPages; pageNumber++) {
+//            String url = "https://oasm.dfsa.dk/uk/searchannouncementresult.aspx?headline=&name=&RealAnnouncerCVR=&CVRnumber=&announcmentsId=-1&informationTypeId=-1&announcementTypeId=-1&languageId=-1&nationality=-1&pubDateFrom=" + dateStart + ":00:00&pubDateTo=" + dateEnd + ":12:21&regDateFrom=1999:01:01:00:00&regDateTo=2050:01:01:00:00&index=" + index + "&sindex=" + sindex + "&historic=1&postponed=0";
+//            String testUrl = "https://oasm.dfsa.dk/uk/searchannouncementresult.aspx?headline=&name=&CVRnumber=&RealAnnouncerCVR=&announcmentsId=-1&informationTypeId=-1&announcementTypeId=-1&languageId=-1&nationality=-1&pubDateFrom=2020:10:15:00:00&pubDateTo=2020:11:12:12:21&regDateFrom=1999:01:01:00:00&regDateTo=2050:01:01:00:00&index=7&sindex=61&historic=1&postponed=0";
+//            try {
+//
+//                Document document = Jsoup.parse(getPageContent(testUrl));
+//                Elements rows = document.select("table>tbody");
+//                if(rows.size()==0) break;
+//
+//                for (Element row : rows) {
+//                    String select = row.select("tr> td:nth-child(1)").text();
+//                    String announcement = row.select("tr.detail> td:nth-child(3) > strong > a").text();
+//                    String announcementLink = row.select("tr.detail > td:nth-child(3)> strong > a").attr("href");
+//                    String company = row.select(" tr.detail > td:nth-child(4)").text();
+//                    String g = "gh";
+//                }
+//                index += 1;
+//                sindex += 10;
+//            } catch (Exception e) {
+//                return;
+//            }
+//        }
+//
+//
+//    }
+//}
