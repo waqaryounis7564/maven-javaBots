cd671a29b4e003af1e71ca3deadd33f9e43b277e
Waqar <waqar.younas7564@gmail.com>
2020-12-17 12:20:12 +0500

    Financial_Services updated for mojirty minorty

--- src/main/java/services/representitives/comittes/Financial_Services.java
+++ src/main/java/services/representitives/comittes/Financial_Services.java
@@ -9,8 +9,11 @@ import java.io.IOException;
 public class Financial_Services {
     public static void scrape() throws IOException {
         Document document = Jsoup.connect("https://financialservices.house.gov/about/committee-membership.htm").get();
-        Elements mixMembers =document.select("#ctl00_ctl22_ctl00_Text > table > tbody>tr>td");
-        mixMembers.forEach(m-> System.out.println(m.text()));
+        Elements majority =document.select("#ctl00_ctl22_ctl00_Text > table > tbody>tr>td:nth-child(odd)");
+        majority.forEach(m-> System.out.println(m.text()));
+        System.out.println("--------------------------------------------");
+        Elements minority =document.select("#ctl00_ctl22_ctl00_Text > table > tbody>tr>td:nth-child(even)");
+        minority.forEach(m-> System.out.println(m.text()));
 
 
 
