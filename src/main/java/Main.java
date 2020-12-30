57abd0182eac1215378a9e91d86f1f47575ec666
Waqar <waqar.younas7564@gmail.com>
2020-12-30 15:47:05 +0500

    korea post request completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,32 +12,13 @@ import java.util.Locale;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        String res = getDayStringOld("2020/12/28");
-        System.out.println(res);
+
+Korea korea=new Korea();
+korea.scrapeData();
+
     }
 
-    public static String getDayStringOld(String date) throws ParseException {
-        String outputDate=null;
-        SimpleDateFormat format1 = new SimpleDateFormat("yyyy/MM/dd");
-        Date dt1 = null;
-        try {
-            dt1 = format1.parse(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        SimpleDateFormat format2 = new SimpleDateFormat("EEEE");
-        String finalDay = format2.format(dt1);
-        Calendar c = Calendar.getInstance();
-        c.setTime(dt1);
-        if (finalDay.equals("Thursday") || finalDay.equals("Friday")) {
-            c.add(Calendar.DATE, 4);
-            outputDate = format1.format(c.getTime());
-        } else {
-            c.add(Calendar.DATE, 2);
-            outputDate = format1.format(c.getTime());
-        }
-        return outputDate;
-    }
+
 
 
 }
--- src/main/java/services/SDS/Korea.java
+++ src/main/java/services/SDS/Korea.java
@@ -2,10 +2,11 @@ package services.SDS;
 
 import models.sds.KoreaDisclosure;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 import java.text.MessageFormat;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,15 +16,66 @@ public class Korea {
     private int count;
 
     public void scrapeData() {
-        try {
-            disclosures = new HashMap<>();
-            readCsvFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        this.disclosures.forEach((k, v) -> System.out.println("key is:" + k + ", and value is" + v.getPositionDate() + v.getShortPositionPercentage()));
+
+        disclosures = new HashMap<>();
+//            readCsvFile();
+        crawlWeb();
+
+        this.disclosures.values().forEach(r -> System.out.println(r.getPositionDate()));
         System.out.println("total counts are:" + count);
     }
+
+    private void crawlWeb() {
+        String content = getContent();
+        System.out.println(content);
+
+    }
+
+    private String getContent() {
+        String response = "";
+//        String dateStart = getDate().get("monthAgo");
+//        String dateEnd = getDate().get("today");
+//        dateStart = MessageFormat.format("''{0}''", dateStart);
+//        dateEnd = MessageFormat.format("''{0}''", dateEnd);
+        String obj = "mkt_tp_cd=1&isu_cdnm=All&isu_cd=&isu_nm=&isu_srt_cd=&strt_dd=20201220&end_dd=20201230&pagePath=%2Fcontents%2FGLB%2F05%2F0501%2F0501120600%2FGLB0501120600.jsp&code=gRv3qxTLY3PfBaFw%2FxSa%2Bk5pUz3yb5NE11wDz1HH3inyEaI8W00Azc4YA2O%2Fbf8h5qKm4V1erZgsOW3mGUBoSjJK5HeSRdABCUvGD9egjtLbBenRvOdipYFFGtSyDN3GLsJIO3Uz0uzVjcCMAdiez0HxaJGdXuklBLh9ntHVM2XyFlg0YnTxIdUWX3%2BZbA7Y";
+        try {
+            URL url = new URL(null,"http://global.krx.co.kr/contents/GLB/99/GLB99000001.jspx", new sun.net.www.protocol.https.Handler());
+            HttpsURLConnection postConnection = (HttpsURLConnection) url.openConnection();
+            postConnection.setConnectTimeout(5000);
+            postConnection.setReadTimeout(10000);
+            postConnection.setRequestMethod("POST");
+            postConnection.setRequestProperty("accept", "application/json, text/javascript, */*; q=0.01");
+            postConnection.setRequestProperty("accept-language", "en-US,en;q=0.9");
+            postConnection.setRequestProperty("content-type", "application/x-www-form-urlencoded; charset=UTF-8");
+            postConnection.setRequestProperty("x-requested-with", "XMLHttpRequest");
+            postConnection.setRequestProperty("cookie", "SCOUTER=z79j613sf2lmhh; __utmz=88009422.1609154622.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmc=88009422; JSESSIONID=6595A39529A245165EB875C463E107CB.103tomcat2; __utma=88009422.1537191816.1609154622.1609310585.1609312508.6; __utmb=88009422.2.10.1609312508");
+            postConnection.setRequestProperty("referrer", "http://global.krx.co.kr/contents/GLB/05/0501/0501120600/GLB0501120600.jsp");
+            postConnection.setRequestProperty("referrerPolicy", "strict-origin-when-cross-origin");
+            postConnection.setRequestProperty("mode", "cors");
+
+            postConnection.setDoOutput(true);
+            OutputStream os = postConnection.getOutputStream();
+            os.write(obj.getBytes());
+            os.flush();
+            os.close();
+            int statusCode = postConnection.getResponseCode();
+            if (statusCode > 299) throw new RuntimeException("BRAZIL RESPONSE CODE :" + statusCode);
+
+            try (InputStream inputStream = postConnection.getInputStream()) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder line = new StringBuilder();
+                String line1;
+                while ((line1 = reader.readLine()) != null) {
+                    line.append(line1);
+                }
+                response = line.toString();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.fillInStackTrace().toString());
+        }
+        return response;
+    }
+
 
     private void readCsvFile() throws IOException {
         String path = "src/main/static/sdsKorea.csv";
@@ -53,7 +105,6 @@ public class Korea {
         korea.setShortPositionPercentage(split[7]);
         disclosures.put(split[0] + split[2], korea);
         count++;
-
 
     }
 }
