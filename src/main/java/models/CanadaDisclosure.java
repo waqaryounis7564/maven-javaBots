c5f5a95d2783a0fc09839b1791390c4737de5022
Waqar <waqar.younas7564@gmail.com>
2021-01-08 13:13:48 +0500

    CandaDisclosure fields type changed

--- src/main/java/models/CanadaDisclosure.java
+++ src/main/java/models/CanadaDisclosure.java
@@ -4,42 +4,13 @@ import java.math.BigDecimal;
 
 public class CanadaDisclosure {
     private String reporting_date;
-
-    public String getPosition_date() {
-        return position_date;
-    }
-
-    public void setPosition_date(String position_date) {
-        this.position_date = position_date;
-    }
-
     private String position_date;
     private String ticker;
     private String issuer_name;
     private String market;
-    private Long short_selling_volume; //
-    private BigDecimal percent_of_total_traded_volume;
-    private Long short_sell_traded_value;
-    private BigDecimal percent_of_total_traded_value;
-    private String url;
-    private Long short_sale_trades;
-    private BigDecimal percent_of_total_trades;
-
-    public Long getShort_sale_trades() {
-        return short_sale_trades;
-    }
-
-    public void setShort_sale_trades(Long short_sale_trades) {
-        this.short_sale_trades = short_sale_trades;
-    }
-
-    public BigDecimal getPercent_of_total_trades() {
-        return percent_of_total_trades;
-    }
-
-    public void setPercent_of_total_trades(BigDecimal percent_of_total_trades) {
-        this.percent_of_total_trades = percent_of_total_trades;
-    }
+    private String short_selling_volume; //
+    private String  percent_of_total_traded_volume;
+    private String  short_sell_traded_value;
 
     public String getReporting_date() {
         return reporting_date;
@@ -47,6 +18,14 @@ public class CanadaDisclosure {
 
     public void setReporting_date(String reporting_date) {
         this.reporting_date = reporting_date;
+    }
+
+    public String getPosition_date() {
+        return position_date;
+    }
+
+    public void setPosition_date(String position_date) {
+        this.position_date = position_date;
     }
 
     public String getTicker() {
@@ -73,36 +52,35 @@ public class CanadaDisclosure {
         this.market = market;
     }
 
-
-    public Long getShort_selling_volume() {
+    public String getShort_selling_volume() {
         return short_selling_volume;
     }
 
-    public void setShort_selling_volume(Long short_selling_volume) {
+    public void setShort_selling_volume(String short_selling_volume) {
         this.short_selling_volume = short_selling_volume;
     }
 
-    public BigDecimal getPercent_of_total_traded_volume() {
+    public String getPercent_of_total_traded_volume() {
         return percent_of_total_traded_volume;
     }
 
-    public void setPercent_of_total_traded_volume(BigDecimal percent_of_total_traded_volume) {
+    public void setPercent_of_total_traded_volume(String percent_of_total_traded_volume) {
         this.percent_of_total_traded_volume = percent_of_total_traded_volume;
     }
 
-    public Long getShort_sell_traded_value() {
+    public String getShort_sell_traded_value() {
         return short_sell_traded_value;
     }
 
-    public void setShort_sell_traded_value(Long short_sell_traded_value) {
+    public void setShort_sell_traded_value(String short_sell_traded_value) {
         this.short_sell_traded_value = short_sell_traded_value;
     }
 
-    public BigDecimal getPercent_of_total_traded_value() {
+    public String getPercent_of_total_traded_value() {
         return percent_of_total_traded_value;
     }
 
-    public void setPercent_of_total_traded_value(BigDecimal percent_of_total_traded_value) {
+    public void setPercent_of_total_traded_value(String percent_of_total_traded_value) {
         this.percent_of_total_traded_value = percent_of_total_traded_value;
     }
 
@@ -113,6 +91,28 @@ public class CanadaDisclosure {
     public void setUrl(String url) {
         this.url = url;
     }
+
+    public String getShort_sale_trades() {
+        return short_sale_trades;
+    }
+
+    public void setShort_sale_trades(String short_sale_trades) {
+        this.short_sale_trades = short_sale_trades;
+    }
+
+    public String getPercent_of_total_trades() {
+        return percent_of_total_trades;
+    }
+
+    public void setPercent_of_total_trades(String percent_of_total_trades) {
+        this.percent_of_total_trades = percent_of_total_trades;
+    }
+
+    private String  percent_of_total_traded_value;
+    private String url;
+    private String  short_sale_trades;
+    private String  percent_of_total_trades;
+
 
 
 }
