332d263fb95d1b68a46d396d1c23a11e3e376fdf
Waqar <waqar.younas@2iqresearch.com>
2021-02-25 17:15:31 +0500

    shadowDom with htmlunit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,38 @@
-import services.sbb.Japan;
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.DomNode;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        downloadXls("https://www.londonstockexchange.com/news-article/DAL/dalata-hotel-group-plc-holding-s-in-company/14875315");
+    }
 
-        try(BufferedReader csvReader = new BufferedReader(new FileReader("src/main/static/zillow.csv"))){
-            String line;
-            csvReader.readLine(); // skipping headers
-            while ((line=csvReader.readLine())!=null){
-                System.out.println(line);
-            }
+    private static void downloadXls(String link) throws IOException {
+        try (WebClient webClient = new WebClient(BrowserVersion.CHROME)) {
+            webClient.getOptions().setJavaScriptEnabled(true);
+            webClient.getOptions().setCssEnabled(false);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+            webClient.getOptions().setRedirectEnabled(true);
+            HtmlPage page = webClient.getPage("https://www.londonstockexchange.com/news-article/DAL/dalata-hotel-group-plc-holding-s-in-company/14875315");
+            Thread.sleep(5000);
+            String javaScriptCode = "return arguments[0].shadowRoot";
+            DomNode domNode = page.querySelector("#news-article-content > div.news-article-content-wrapper > div.news-article-content-body");
+            Object javaScriptResult =  page.executeJavaScript("return arguments[0].shadowRoot", String.valueOf(domNode),1).getJavaScriptResult();
+            System.out.println(javaScriptResult);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-//                Japan.scrape();
 
     }
 
-
+    private static void processLine(String line) {
+        System.out.println(line);
+    }
 }
+
+
