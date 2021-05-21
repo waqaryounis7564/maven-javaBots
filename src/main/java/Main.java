7f805701181f970370849891a356466da1e3868d
Waqar <waqar.younas7564@gmail.com>
2021-05-21 09:59:37 +0500

    finland implemented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,17 @@
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import services.common.CssDataNullException;
 import services.sbb.Finland;
-
-import javax.net.ssl.HttpsURLConnection;
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.MessageFormat;
-import java.util.*;
 
 public class Main {
 
 
     public static void main(String[] args) throws Exception {
         Finland finland=new Finland();
-        finland.crawl();
+        finland.startCrawling();
 
-        }
     }
 
 
 
+
+}
 
 
